3601070b2ed64166bbf798670f49e0a53fb86765
adityamogadala <aditya.mogadala@gmail.com>
2016-05-10 19:11:32 +0200

    bug fix languages comparedocs

--- docsim/CompareTwoDocs.py
+++ docsim/CompareTwoDocs.py
@@ -17,7 +17,7 @@ class CompareDocContent:
 	def __init__(self):
 		self.wordvecdir='./StoreWordVec/wiki/'
 	def detectlang(self,text):
-		langid.set_languages(['en','de','es','it'])
+	#	langid.set_languages(['en','de','es','it'])
 		return langid.classify(text)[0]
 	def getvector(self,text,shelvedb):
 		doc_vector=np.array([0.0]*100)
@@ -35,19 +35,67 @@ class CompareDocContent:
                         final_vector = doc_vector
                 return final_vector		
 	def monolingualcomp(self,lang1,text1,text2):
-		s = shelve.open(self.wordvecdir+'en_de_vec_wiki_shelf.db')
+		## Monolingual Word Embeddings ##
+		if lang1=='en':
+			s = shelve.open(self.wordvecdir+'en_en_vec_wiki_shelf.db')
+		elif lang1=='de':
+			s = shelve.open(self.wordvecdir+'de_de_vec_wiki_shelf.db')
+		elif lang1=='es':
+			s = shelve.open(self.wordvecdir+'es_es_vec_wiki_shelf.db')
+		elif lang1=='it':
+			s = shelve.open(self.wordvecdir+'it_it_vec_wiki_shelf.db')
 		text1_vec = self.getvector(text1,s)
 		text2_vec = self.getvector(text2,s)
 		s.close()
 		d = distance.cdist(np.array([text1_vec]),np.array([text2_vec]), 'cosine')
 		return 1.0-d[0][0]
 	def crosslingualcomp(self,lang1,text1,lang2,text2):
-		s_lang1 = shelve.open(self.wordvecdir+'en_de_vec_wiki_shelf.db')
-		text1_vec = self.getvector(text1,s_lang1)
-		s_lang1.close()
-		s_lang2 = shelve.open(self.wordvecdir+'de_en_vec_wiki_shelf.db')
-		text2_vec = self.getvector(text2,s_lang2)
-		s_lang2.close()
+		## Bilingual Word Embeddings ##
+		if lang1=='en' and lang2=='de':
+			s_lang1 = shelve.open(self.wordvecdir+'en_de_vec_wiki_shelf.db')
+			text1_vec = self.getvector(text1,s_lang1)
+			s_lang1.close()
+			s_lang2 = shelve.open(self.wordvecdir+'de_en_vec_wiki_shelf.db')
+			text2_vec = self.getvector(text2,s_lang2)
+			s_lang2.close()
+		elif lang1=='de' and lang2=='en':
+			s_lang1 = shelve.open(self.wordvecdir+'de_en_vec_wiki_shelf.db')
+			text1_vec = self.getvector(text1,s_lang1)
+			s_lang1.close()
+			s_lang2 = shelve.open(self.wordvecdir+'en_de_vec_wiki_shelf.db')
+			text2_vec = self.getvector(text2,s_lang2)
+			s_lang2.close()
+		elif lang1=='en' and lang2=='it':
+			s_lang1 = shelve.open(self.wordvecdir+'en_it_vec_wiki_shelf.db')
+			text1_vec = self.getvector(text1,s_lang1)
+			s_lang1.close()
+			s_lang2 = shelve.open(self.wordvecdir+'it_en_vec_wiki_shelf.db')
+			text2_vec = self.getvector(text2,s_lang2)
+			s_lang2.close()
+		elif lang1=='it' and lang2=='en':
+			s_lang1 = shelve.open(self.wordvecdir+'it_en_vec_wiki_shelf.db')
+			text1_vec = self.getvector(text1,s_lang1)
+			s_lang1.close()
+			s_lang2 = shelve.open(self.wordvecdir+'en_it_vec_wiki_shelf.db')
+			text2_vec = self.getvector(text2,s_lang2)
+			s_lang2.close()
+		elif lang1=='en' and lang2=='es':
+			s_lang1 = shelve.open(self.wordvecdir+'en_es_vec_wiki_shelf.db')
+			text1_vec = self.getvector(text1,s_lang1)
+			s_lang1.close()
+			s_lang2 = shelve.open(self.wordvecdir+'es_en_vec_wiki_shelf.db')
+			text2_vec = self.getvector(text2,s_lang2)
+			s_lang2.close()
+		elif lang1=='es' and lang2=='en':
+			s_lang1 = shelve.open(self.wordvecdir+'es_en_vec_wiki_shelf.db')
+			text1_vec = self.getvector(text1,s_lang1)
+			s_lang1.close()
+			s_lang2 = shelve.open(self.wordvecdir+'en_es_vec_wiki_shelf.db')
+			text2_vec = self.getvector(text2,s_lang2)
+			s_lang2.close()
+		else:
+			print 'Currently, this combination is not supported. Only between and other EU languages (de,es,it).'
+			sys.exit(1)
 		d = distance.cdist(np.array([text1_vec]),np.array([text2_vec]), 'cosine')
 		return 1.0-d[0][0]
 	def compare(self,text1,text2):
@@ -64,5 +112,5 @@ class CompareDocContent:
 				print 'Cross-Lingual Document Comparison..... '
 				vals = self.crosslingualcomp(lang1,text1,lang2,text2)
 		else:
-			print 'currently we dont support this language. Only between EN and Other EU languages'
+			print 'Currently we dont support this language. Only en,de,it and es.'
 		return vals
--- examples/testcomparedocs.py
+++ examples/testcomparedocs.py
@@ -4,9 +4,11 @@ sys.path.insert(0, '../docsim')
 import CompareTwoDocs
 
 text1 = "I do not speak english"
-text2 = "München bietet in seinen diversen Abteilungen immer"
+#text1 = "München bietet in seinen diversen Abteilungen immer"
+text2="es usted estoy bien dónde está Berlín"
+#text1='wie zijt gij, ik ben goed , waar is Berlijn'
 #text2 = "A statement from Mr Obama's press secretary read: The President will make an historic visit to Hiroshima with Prime Minister [Shinzo] Abe to highlight his continued commitment to pursuing peace and security in a world without nuclear weapons."
 comparedoc = CompareTwoDocs.CompareDocContent()
 score = comparedoc.compare(text1,text2)
-print 'Score between 0 and 1: ', score
+print '[Score between 0 and 1]: ', score
 

d1aa59d7223550a9d392d734f2fb15c31937da7b
adityamogadala <aditya.mogadala@gmail.com>
2016-05-13 18:52:48 +0200

    added example to count

---
+++ examples/countcollectiondocs.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import pymongo
+from pymongo import MongoClient
+class CountMongo:
+	def __init__(self,configdic):
+		self.configdict = configdic
+	def count(self):
+		if self.configdict['MongoDBPath']!="":
+			client = MongoClient(self.configdict['MongoDBPath'])
+			if self.configdict['MongoDBUserName']!="" and self.configdict['MongoDBPassword']!="":
+				client.the_database.authenticate(self.configdict['MongoDBUserName'],self.configdict['MongoDBPassword'],source=self.configdict['MongoDBStorage'])
+				storedb = client[self.configdict['MongoDBStorage']]
+				collection,collection1,collection2,collection3,collection4 = storedb[self.configdict['KafkaTopicTVMetadata']], storedb[self.configdict["KafkaTopicSocialMedia"]],storedb[self.configdict['KafkaTopicNews']],storedb[self.configdict['KafkaTopicASR']],storedb[self.configdict['KafkaTopicSubtitles']]
+	
+		print "Total Docs in Collection [tvmetadata], [socialmedia],[jsinewsfeed],[zattooasr],[ZattooSubTitles]:: ", collection.find().count(), collection1.find().count(), collection2.find().count(), collection3.find().count(),collection4.find().count()
+		
+def main():
+	configdict={}
+        config = '../config/Config.conf'
+        with open(config) as config_file:
+                for lines in config_file:
+                        if re.search(r'=',lines):
+                                key = lines.strip('\n').strip().split('=')
+                                configdict[key[0]]=key[1]
+	testmongo = CountMongo(configdict)
+	testmongo.count()
+if __name__ == "__main__":
+	main()
